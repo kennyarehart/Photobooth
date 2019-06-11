0e931fb49f1fd96a8a0f32b11d78b4ad1d99fdcb
Kenny Arehart <kenny.arehart@ff0000.com>
2019-06-11 16:38:44 -0700

    Initial Commit

---
+++ Custom Camera/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Custom Camera
+//
+//  Created by Kenny Arehart on 6/11/19.
+//  Copyright © 2019 Kenny Arehart. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
